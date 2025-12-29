df4be63e8316b81dd84ca826f88bdb4ea22fb4e9
SaffatHasan <saffathasan@gmail.com>
2025-12-29 08:39:02 +0000

    Deploying to gh-pages from @ SaffatHasan/rust-tac-toe@e10db019c63637edc4574c39e5712158e196666e 🚀

--- gui-wasm/rust-tac-toe-gui-wasm-d84d6da97b095c30.js
+++ gui-wasm/rust-tac-toe-gui-wasm-d84d6da97b095c30.js
@@ -292,6 +292,14 @@ if (!('encodeInto' in cachedTextEncoder)) {
 
 let WASM_VECTOR_LEN = 0;
 
+function wasm_bindgen__convert__closures_____invoke__h4c3dfc33db0f0560(arg0, arg1, arg2) {
+    wasm.wasm_bindgen__convert__closures_____invoke__h4c3dfc33db0f0560(arg0, arg1, arg2);
+}
+
+function wasm_bindgen__convert__closures_____invoke__hd597628a407cebe0(arg0, arg1, arg2) {
+    wasm.wasm_bindgen__convert__closures_____invoke__hd597628a407cebe0(arg0, arg1, arg2);
+}
+
 function wasm_bindgen__convert__closures_____invoke__h82fe2553ad2b6423(arg0, arg1) {
     const ret = wasm.wasm_bindgen__convert__closures_____invoke__h82fe2553ad2b6423(arg0, arg1);
     if (ret[1]) {
@@ -299,16 +307,8 @@ function wasm_bindgen__convert__closures_____invoke__h82fe2553ad2b6423(arg0, arg
     }
 }
 
-function wasm_bindgen__convert__closures_____invoke__hd597628a407cebe0(arg0, arg1, arg2) {
-    wasm.wasm_bindgen__convert__closures_____invoke__hd597628a407cebe0(arg0, arg1, arg2);
-}
-
 function wasm_bindgen__convert__closures_____invoke__h4c6b928d3ccd58aa(arg0, arg1) {
     wasm.wasm_bindgen__convert__closures_____invoke__h4c6b928d3ccd58aa(arg0, arg1);
-}
-
-function wasm_bindgen__convert__closures_____invoke__h4c3dfc33db0f0560(arg0, arg1, arg2) {
-    wasm.wasm_bindgen__convert__closures_____invoke__h4c3dfc33db0f0560(arg0, arg1, arg2);
 }
 
 export function start_app() {
